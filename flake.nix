501e18428e483e5960728fc8b24637cf4f6586a3
Lucas Denton <ladent99@gmail.com>
2026-05-05 20:19:33 -0400

    update

--- flake.nix
+++ flake.nix
@@ -11,18 +11,10 @@
           packages = with pkgs; [
             nodejs_22
             typescript-language-server
-            prisma
-            prisma-engines
-            openssl
             postgresql
           ];
           shellHook = ''
             echo "Node $(node --version)"
-            export PKG_CONFIG_PATH="${pkgs.openssl.dev}/lib/pkgconfig"
-            export PRISMA_SCHEMA_ENGINE_BINARY="${pkgs.prisma-engines}/bin/schema-engine"
-            export PRISMA_QUERY_ENGINE_BINARY="${pkgs.prisma-engines}/bin/query-engine"
-            export PRISMA_QUERY_ENGINE_LIBRARY="${pkgs.prisma-engines}/lib/libquery_engine.node"
-            export PRISMA_FMT_BINARY="${pkgs.prisma-engines}/bin/prisma-fmt"
           '';
         };
       });
